978decf03b188612b649074f9691cd447d4da1aa
Tatiana2101 <7987112@gmail.com>
2025-01-17 11:35:18 +0300

    Update Fruit_task.py

--- Adam_task (5).py
+++ Adam_task (5).py
@@ -1,43 +1,31 @@
-# In[7]:
-from tensorflow.keras.datasets import fashion_mnist
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, AvgPool2D
-from tensorflow.keras.models import Sequential
-import numpy as np
-from tensorflow.keras.optimizers import Adam
+def  load_train(path):
+        datagen = ImageDataGenerator(
+        horizontal_flip=True,
+        vertical_flip=True,
+        rescale=1/255.)
 
-# In[16]:
+        train_datagen_flow = datagen.flow_from_directory(
+        path,
+        target_size=(150, 150),
+        batch_size=16,
+        class_mode='sparse',
+        seed=12345)
 
-def load_train(path):
-    features_train = np.load(path + 'train_features.npy')
-    target_train = np.load(path + 'train_target.npy')
-    features_train = features_train.reshape(-1, 28, 28, 1) / 255.0   
-    return features_train, target_train 
+        return train_datagen_flow
 
-# In[14]:
-
-def create_model(input_shape):
-    model = Sequential()
-    model.add(Conv2D(filters=6, kernel_size=(5, 5), padding='same',
-                     activation="relu", input_shape=input_shape))
-    model.add(Flatten())
-    model.add(Dense(10, activation='softmax'))
-    optimizerAd = Adam(learning_rate=0.001)
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizerAd,
-                      metrics=['acc'])
-    return model 
-
-# In[15]:
-
-def train_model(model, train_data, test_data, batch_size=16, epochs=5,
+def train_model(model, train_data, test_data, batch_size=None, epochs=10,
                 steps_per_epoch=None, validation_steps=None):
 
-    features_train, target_train = train_data
-    features_test, target_test = test_data
-    model.fit(features_train, target_train, 
-              validation_data=(features_test, target_test),
+    if steps_per_epoch is None:
+        steps_per_epoch = len(train_data)
+    if validation_steps is None:
+        validation_steps = len(test_data)
+
+    model.fit(train_data,
+              validation_data=test_data,
               batch_size=batch_size, epochs=epochs,
               steps_per_epoch=steps_per_epoch,
               validation_steps=validation_steps,
-              verbose=2, shuffle=True)
+              verbose=2)
 
     return model
