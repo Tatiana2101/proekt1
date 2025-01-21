2a956d6c8c11fb2abdaa0e4bfebeea835307fb31
Tatiana2101 <7987112@gmail.com>
2025-01-21 09:30:37 +0300

    Update Adam_task (5).py

--- Adam_task (5).py
+++ Adam_task (5).py
@@ -1,51 +1,34 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, AveragePooling2D, Flatten, Dense
 
-def load_train(path):
-    datagen = ImageDataGenerator(
+def  load_train(path):
+        datagen = ImageDataGenerator(
         horizontal_flip=True,
         vertical_flip=True,
-        rescale=1./255.)
+        rescale=1/255.)
 
-    train_datagen_flow = datagen.flow_from_directory(
+        train_datagen_flow = datagen.flow_from_directory(
         path,
         target_size=(150, 150),
         batch_size=16,
         class_mode='sparse',
         seed=12345)
 
-    return train_datagen_flow
+        return train_datagen_flow
 
 def create_model(input_shape):
+    backbone = ResNet50(input_shape=(150, 150, 3),
+                    weights='/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                    include_top=False)
+
     model = Sequential()
-    model.add(Conv2D(filters=6,
-                     kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=input_shape))
-    model.add(AveragePooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(units=12, activation='softmax'))
+    model.add(backbone)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(12, activation='softmax'))
+
     model.compile(loss='sparse_categorical_crossentropy', 
-                  optimizer='adam', metrics=['acc'])
+              optimizer='adam', metrics=['acc'])
     return model
-
-def train_model(model, train_data, test_data, batch_size=None, epochs=10,
-                steps_per_epoch=None, validation_steps=None):
-
-    if steps_per_epoch is None:
-        steps_per_epoch = len(train_data)
-    if validation_steps is None:
-        validation_steps = len(test_data)
-
-    model.fit(train_data,
-              validation_data=test_data,
-              batch_size=batch_size, epochs=epochs,
-              steps_per_epoch=steps_per_epoch,
-              validation_steps=validation_steps,
-              verbose=2)
-
-    return model
-
 
 
