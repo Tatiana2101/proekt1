3100e38ea9a89555d7fdb9c6a7615fa0937e201d
Tatiana2101 <7987112@gmail.com>
2025-01-21 09:40:00 +0300

    Update resnet.py

--- Adam_task (5).py
+++ Adam_task (5).py
@@ -25,6 +25,36 @@ def create_model(input_shape):
 
     model.compile(loss='sparse_categorical_crossentropy', 
               optimizer='adam', metrics=['acc'])
+    return modelfrom tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Sequential
+def  load_train(path):
+        datagen = ImageDataGenerator(
+        horizontal_flip=True,
+        vertical_flip=True,
+        rescale=1/255.)
+
+        train_datagen_flow = datagen.flow_from_directory(
+        path,
+        target_size=(150, 150),
+        batch_size=16,
+        class_mode='sparse',
+        seed=12345)
+
+        return train_datagen_flow
+
+def create_model(input_shape):
+    backbone = ResNet50(input_shape=(150, 150, 3),
+                    weights='/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                    include_top=False)
+
+    model = Sequential()
+    model.add(backbone)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(12, activation='softmax'))
+
+    model.compile(loss='sparse_categorical_crossentropy', 
+              optimizer='adam', metrics=['acc'])
     return model
+
 
 
